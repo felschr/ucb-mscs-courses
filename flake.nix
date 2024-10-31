2fc91b17ffc7140cafe0b6afcee215786800efc4
Felix Schröter <dev@felschr.com>
2024-10-31 21:30:44 +0100

    build: add simple Nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+        ./nix/overlays.nix
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devenv.shells.default = {
+            dotenv.disableHint = true;
+
+            stdenv = if pkgs.stdenv.isDarwin then pkgs.stdenvNoCC else pkgs.stdenv;
+
+            packages = with pkgs; [
+              gitMinimal
+              nushell
+
+              # language servers
+              nil
+            ];
+
+            containers = pkgs.lib.mkForce { };
+          };
+        };
+    };
+}
