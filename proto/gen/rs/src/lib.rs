ea4273e2e3db473678f71eddba0945ad7de38277
Felix Schröter <dev@felschr.com>
2024-11-02 19:18:23 +0100

    chore(proto): add generated code

---
+++ proto/gen/rs/src/lib.rs
@@ -0,0 +1,9 @@
+// @generated
+pub mod course {
+    #[cfg(feature = "course-v1")]
+    // @@protoc_insertion_point(attribute:course.v1)
+    pub mod v1 {
+        include!("course.v1.rs");
+        // @@protoc_insertion_point(course.v1)
+    }
+}
